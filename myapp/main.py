ce14bf7fb5c6032be337dc8ad3b617f7a67a5048
esnyder <es@example.com>
2024-10-23 14:31:53 -0500

    Updating project

--- myapp/main.py
+++ myapp/main.py
@@ -4,6 +4,13 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from prometheus_client import make_asgi_app, Counter
 
+from pymongo import MongoClient
+from pymongo.server_api import ServerApi
+
+import os
+
+mongo_uri = os.environ.get("MONGO_CONNECTION_STRING")
+
 templates = Jinja2Templates(directory="templates")
 
 app = FastAPI()
@@ -12,9 +19,18 @@ app.mount("/scripts", StaticFiles(directory="scripts"), name="scripts")
 
 metrics_app = make_asgi_app()
 hello_counter = Counter("worlds_helloed", "Total number of worlds we have helloed")
+reviews_served = Counter("reviews_served", "Total number of reviews served")
+
 app.mount("/metrics", metrics_app)
+
 
 @app.get("/", response_class=HTMLResponse)
 async def index(request: Request):
     hello_counter.inc()
     return templates.TemplateResponse(name="index.html", request=request)
+
+
+@app.get("/review", response_class=HTMLResponse)
+async def index(request: Request):
+    reviews_served.inc()
+    return templates.TemplateResponse(name="review.html", request=request)
